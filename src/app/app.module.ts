9d215cbc1e9c468c87af757e06af3096cc090215
jaime <lockdown97140@gmail.com>
2024-04-23 22:55:42 -0600

    Inicio de TypeORM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin',
+      database: 'test2',
+      entities: [],
+      //No utilizar "synchronize: true" en prod
+      synchronize: true,
+      retryAttempts: 5,
+      retryDelay: 3000,
+      autoLoadEntities: true
+    }),
+    AuthModule],
   controllers: [],
   // providers: [AppService],
 })
