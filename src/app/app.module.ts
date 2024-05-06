e8e84584c4cd507d4e0a06d7350c2ca61742112b
jaime <lockdown97140@gmail.com>
2024-05-05 22:28:30 -0600

    CRUD libro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
-import { Credenciales } from 'src/credenciales/credenciales.entity';
 import { CredencialesModule } from 'src/credenciales/credenciales.module';
+import { LibroModule } from 'src/libro/libro.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'admin',
-      database: 'test2',
-      entities: ["dist/**/*.entity{.ts,.js}"],
-      //No utilizar "synchronize: true" en prod
-      synchronize: true,
-      retryAttempts: 5,
-      retryDelay: 3000,
-      autoLoadEntities: true
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
     }),
-    AuthModule, CredencialesModule],
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: ["dist/**/*.entity{.ts,.js}"],
+        //No utilizar "synchronize: true" en prod
+        synchronize: true,
+        retryAttempts: 5,
+        retryDelay: 3000,
+        autoLoadEntities: true
+      }),
+    }),
+    AuthModule, CredencialesModule, LibroModule],
   controllers: [],
   // providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
